7f707f8c78282107c6f97c40a172172c849e06c8
vaibhavantil2 <88091026+vaibhavantil2@users.noreply.github.com>
2024-04-09 09:16:17 -0400

    Update gtm.tsx

--- apps/store/src/services/gtm.tsx
+++ apps/store/src/services/gtm.tsx
@@ -112,14 +112,14 @@ export const GTMBodyScript = () => {
     </noscript>
   )
 }
-
+/*
 export const TikTokPixelScript = () => {
   const TIKTOK_PIXEL_ID = "YOUR_TIKTOK_PIXEL_ID"; // Replace with your actual TikTok Pixel ID
 
   if (!TIKTOK_PIXEL_ID) return null;
 
   return (
-    <Script
+   <Script
       id="tiktok-pixel-base"
       strategy="lazyOnload"
       dangerouslySetInnerHTML={{
@@ -137,7 +137,7 @@ export const TikTokPixelScript = () => {
     />
   );
 }; 
-
+*/
 export type AppTrackingContext = {
   countryCode: CountryCode
 }
